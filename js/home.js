ee6c6ced63b01b13a00fd5e1ebd09863d7dbbc96
Deividas <dgdeivis116@gmail.com>
2021-04-26 19:48:05 +0300

    Preparation

--- js/home.js
+++ js/home.js
@@ -1,3 +1,36 @@
 // COMPONENTS AND DATA IMPORT
 
+/* Header start */
+/* Header end */
+
+/* Introduction start */
+/* Introduction end */
+
+/* Personal details start */
+/* Personal details end */
+
+/* Offered services start */
+/* Offered services end */
+
+/* Information bar start */
+/* Information bar end */
+
+/* Featured projects start */
+/* Featured projects end */
+
+/* Feedback start */
+/* Feedback end */
+
+/* Choose plan start */
+/* Choose plan end */
+
+/* Blog posts start */
+/* Blog posts end */
+
+/* Advertise bar start */
+/* Advertise bar end */
+
+/* Footer start */
+/* Footer end */
+
 // COMPONENTS EXECUTION
