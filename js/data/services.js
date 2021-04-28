1b3d27385508d3108b177f9d370cad260384e956
Aldas <aldevinas.kasputis@gmail.com>
2021-04-28 21:50:44 +0300

    js files created

---
+++ js/data/services.js
@@ -0,0 +1,3 @@
+const servicesInfo = []
+
+export { servicesInfo }
--- js/home.js
+++ js/home.js
@@ -10,6 +10,8 @@
 /* Personal details end */
 
 /* Offered services start */
+import { servicesInfo } from './data/services.js';
+import { offeredservices } from './components/offeredservices.js';
 /* Offered services end */
 
 /* Information bar start */
