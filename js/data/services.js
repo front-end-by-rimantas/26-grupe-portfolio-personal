2327756aca1d851b87ec21e166b513ba0ec2c339
Aldas <aldevinas.kasputis@gmail.com>
2021-04-29 00:31:09 +0300

    text rendex with js

--- js/data/services.js
+++ js/data/services.js
@@ -1,31 +1,31 @@
 const servicesInfo = [
     { 
-        icon: "a",
+        icon: "fas fa-chart-pie",
         title: "Web Design",
         text: "It is not because things are difficult that we do not dare; it is because we do not dare that they are difficult." 
     },
     { 
-        icon: "b",
+        icon: "fas fa-laptop",
         title: "Web Development",
         text: "If you are an entrepreneur, you know that your success cannot depend on the opinions of others. Like the wind, opinions." 
     },
     { 
-        icon: "c",
+        icon: "fas fa-camera",
         title: "Photography",
         text: "Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills." 
     },
     { 
-        icon: "d",
+        icon: "far fa-image",
         title: "Clipping Path",
         text: "Hypnosis quit smoking methods maintain caused quite a stir in the medical world over the last two decades. There is a lot of argument." 
     },
     { 
-        icon: "e",
+        icon: "fas fa-tablet-alt",
         title: "Apps Interface",
         text: "Do you sometimes have the feeling that you’re running into the same obstacles over and over again? Many of my conflicts." 
     },
     { 
-        icon: "f",
+        icon: "fas fa-rocket",
         title: "Graphic Design",
         text: "You’ve heard the expression, “Just believe it and it will come.” Well, technically, that is true, however, ‘believing’ is not just thinking that."
     },
