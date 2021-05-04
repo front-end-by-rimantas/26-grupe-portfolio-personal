a54618c1921f939800fd066187477126a319db54
Kerbelyte <80693721+Kerbelyte@users.noreply.github.com>
2021-05-04 10:18:13 +0300

    fix

--- js/components/advertiseBar.js
+++ js/components/advertiseBar.js
@@ -4,7 +4,9 @@ function renderLogos(data) {
     let HTML = '';
 
     for (let i = 0; i < data.length; i++) {
-        HTML += `<img class= "logo" src="${data[i].img}" alt="">`
+        HTML += `<div class="soloBrand">
+                    <img src="${data[i].img}" alt="${data[i].name}">
+                </div>`
 
     }
     DOM.innerHTML = HTML;
--- js/data/advertiseData.js
+++ js/data/advertiseData.js
@@ -1,19 +1,24 @@
 const logosData = [{
+        name: 'l1',
         img: './img/logo/l1.png',
     },
     {
+        name: 'l2',
         img: './img/logo/l2.png',
 
     },
     {
+        name: 'l3',
         img: './img/logo/l3.png',
 
     },
     {
+        name: 'l4',
         img: './img/logo/l4.png',
 
     },
     {
+        name: 'l5',
         img: './img/logo/l5.png',
     }
 ]
